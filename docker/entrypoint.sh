ff1225c27db7fe35e1409e36ffcc5ea197787f7a
Amir <zamnatov2004@gmail.com>
2026-03-20 10:53:54 +0300

    fixed wrong port in docker entrypoint and .env.docker.example

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,13 +2,13 @@
 set -eu
 
 HOST="${APP_HOST:-0.0.0.0}"
-PORT="${APP_PORT:-8000}"
+PORT="${CONTAINER_APP_PORT:-8000}"
 
 case "${DEBUG:-false}" in
   [Tt]rue|1)
-    exec uvicorn src.main:app --host "$HOST" --port "$PORT" --reload
+    exec uvicorn src.main:app --host "$HOST" --port "$PORT" --reload --proxy-headers --forwarded-allow-ips="*"
     ;;
   *)
-    exec uvicorn src.main:app --host "$HOST" --port "$PORT"
+    exec uvicorn src.main:app --host "$HOST" --port "$PORT" --proxy-headers --forwarded-allow-ips="*"
     ;;
 esac
